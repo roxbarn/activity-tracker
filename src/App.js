c40fab8a851861707a01e09340e04ec9ec01f01b
Roxanne Barney <roxanne_barney@hotmail.com>
2020-05-24 11:41:52 +0800

    sign in styling

--- src/App.js
+++ src/App.js
@@ -1,16 +1,22 @@
 import React from "react";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import "./App.css";
+import SignIn from "./pages/SignIn";
+
+import { ThemeProvider } from "@material-ui/core/styles";
+import theme from "./config/theme.config";
 
 function App() {
   return (
     <Router>
-      <Switch>
-        <Route exact path="/">
-          Sign in
-        </Route>
-        <Route path="/sign-up">Sign up</Route>
-      </Switch>
+      <ThemeProvider theme={theme}>
+        <Switch>
+          <Route exact path="/">
+            <SignIn />
+          </Route>
+          <Route path="/sign-up">Sign up</Route>
+        </Switch>
+      </ThemeProvider>
     </Router>
   );
 }
---
+++ src/config/theme.config.jsx
@@ -0,0 +1,17 @@
+import { createMuiTheme } from "@material-ui/core/styles";
+
+const theme = createMuiTheme({
+  typography: {
+    fontFamily: "Piedra",
+  },
+  palette: {
+    primary: {
+      main: "#4D243D",
+    },
+    secondary: {
+      main: "#8ec5fc",
+    },
+  },
+});
+
+export default theme;
